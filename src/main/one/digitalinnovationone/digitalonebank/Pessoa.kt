b4ea459473f263ae8ca2d0e8223eb0646aa7ab87
William Daniel <wdarchenemy@gmail.com>
2021-02-15 05:53:37 -0300

    Adição do contrutor padrão e função  pessoInfo

--- src/main/one/digitalinnovationone/digitalonebank/Pessoa.kt
+++ src/main/one/digitalinnovationone/digitalonebank/Pessoa.kt
@@ -5,16 +5,18 @@ class Pessoa{
     var cpf = "132.315.426-50"
     private set
 
+    constructor()
+
     inner class Endereco{
         var rua : String = "15 de Novembro"
     }
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main(){
     val pessoa = Pessoa()
 
-    println(pessoa.nome)
-    println(pessoa.cpf)
-    println(pessoa.Endereco().rua)
+    println(pessoa.pessoaInfo())
 
 }
